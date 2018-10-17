007221132056ec61c7a7d5f94e36ee2c28e6d931
Dale McDiarmid <dalem@elastic.co>
2018-10-17 12:07:49 +0100

    Revert build

--- frontend/client/webpack.config.js
+++ frontend/client/webpack.config.js
@@ -92,14 +92,4 @@ var config = {
    }
 };
 
-if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production'),
-            }
-        })
-    )
-}
-
 module.exports = config;
